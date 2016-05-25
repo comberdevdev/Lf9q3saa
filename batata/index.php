00fef23dd540b7aa75c928d96c6e9eb508275768
Leonardo G Mello <leo.galetti@gmail.com>
2016-05-25 17:10:44 -0300

    ALterações do Nicolas

--- batata/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require '../includes/config.php';
-	require $caminhoFisico . '/includes/head.php';
-	require $caminhoFisico . '/includes/header.php';
-	require 'conteudo.php';
-	require $caminhoFisico . '/includes/footer.php';
-?>    
