773a04a245983531f24c2327d70db3edba590771
SampaioO <nicolas@comberweb.com.br>
2016-06-01 16:52:12 -0300

    alteracoes_item_contatos

--- modulos/formulario/contatos.php
+++ modulos/formulario/contatos.php
@@ -15,7 +15,7 @@
                             	<tr>
                                     <th>Email</th>
                                     <th>Assunto</th>
-                                    <th>Detalhes</th>
+                                    <th><center>Detalhes</center></th>
                                     <th><center>Arquivo</center></th>
                                 </tr>
                             </thead>
@@ -23,10 +23,14 @@
                                 <tr>
                                     <td>paulo@comberweb.com.br</td>
                                     <td>Assunto teste 01</td>
-                                    <td>&nbsp;</td>
                                     <td>
                                     	<center>
-                                    		<button type="button" class="btn btn-default btn-arquivo"  data-toggle="modal" data-target="#modalImagem"><span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;Visualizar arquivo</button>
+                                    		<button type="button" class="btn btn-default btn-dados" data-toggle="modal" data-target="#modalDados"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;&nbsp;Visualizar Dados</button>
+                                        </center>
+                                    </td>
+                                    <td>
+                                    	<center>
+                                    		<button type="button" class="btn btn-default btn-arquivo"  data-toggle="modal" data-target="#modalArquivo"><span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;Visualizar arquivo</button>
                                         </center>
                                     </td>
                                 </tr>
@@ -34,14 +38,22 @@
                                 <tr>
                                     <td>nicolas@comberweb.com.br</td>
                                     <td>Assunto teste 02</td>
-                                    <td>&nbsp;</td>
+                                    <td>
+                                    	<center>
+                                    		<button type="button" class="btn btn-default btn-dados" data-toggle="modal" data-target="#modalDados"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;&nbsp;Visualizar dados</button>
+                                        </center>
+                                    </td>
                                     <td>&nbsp;</td>
                                 </tr>
                                 
                                 <tr>
                                     <td>teste@teste</td>
                                     <td>Assunto teste 03</td>
-                                    <td>&nbsp;</td>
+                                    <td>
+                                    	<center>
+                                    		<button type="button" class="btn btn-default btn-dados" data-toggle="modal" data-target="#modalDados"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;&nbsp;Visualizar dados</button>
+                                        </center>
+                                    </td>
                                     <td>
                                     	<center>
                                     		<button type="button" class="btn btn-default btn-arquivo" data-toggle="modal" data-target="#modalArquivo"><span class="glyphicon glyphicon-file"></span>&nbsp;&nbsp;Visualizar arquivo</button>
@@ -63,19 +75,33 @@
     </div>
 </div>
 
+<div class="modal fade" id="modalDados" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn btn-default btn-modal-close pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span></button>
+            </div>
+            <div class="modal-body">
+                <h4>Email: Teste@teste.com.br</h4>
+                <hr>
+                <h4>Cc: Example@Example.com</h4>
+                <hr>
+                <h4>Assunto: Lorem Ipsum</h4>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at finibus ipsum. Aenean pulvinar, purus sed ornare faucibus, libero sem varius velit, et imperdiet lorem orci vitae sapien. Maecenas ultricies faucibus turpis nec porttitor. Proin eleifend non magna et lacinia. Nullam a metus dapibus, finibus libero vitae, lobortis velit. Suspendisse potenti. Morbi placerat, magna ac dictum placerat, nisl ex vestibulum augue, sit amet dignissim enim dolor sit amet lectus. Ut ut purus a nibh laoreet ullamcorper. Fusce pharetra lorem at ipsum aliquam viverra. Duis convallis, mauris eu commodo aliquet, sem lacus auctor erat, sed blandit justo orci eu lectus. Curabitur ullamcorper eu lacus et lacinia.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" id="modalArquivo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <button type="button" class="btn btn-default btn-modal-close pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span></button>
             </div>
             <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+               <iframe src="uploads/teste.pdf" style="zoom:0.60" width="99.6%" height="1000" frameborder="0"></iframe>
             </div>
         </div>
     </div>
